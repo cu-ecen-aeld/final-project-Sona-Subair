4e11f93445b9c4f08e9530f36d0fe3083677a37d
suryakanteti <suryateja.kanteti@gmail.com>
2022-11-14 08:41:29 +0000

    Working buildroot from beaglebone base repo

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ cd `dirname $0`
 
 if [ ! -e buildroot/.config ]
 then
-	echo "MISSING BUILDROOT CONFIGURATION FILE"
+	echo -e "\n MISSING BUILDROOT CONFIGURATION FILE \n"
 
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
@@ -23,11 +23,13 @@ then
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
+		echo "If this is your first time building for a harware platform you might want to consider using your"
+		echo "qemu defconfig at ${QEMU_MODIFIED_DEFCONFIG} as a reference"
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
 	fi
 else
-	echo "USING EXISTING BUILDROOT CONFIG"
-	echo "To force update, delete .config or make changes using make menuconfig and build again."
+	echo "\n USING EXISTING BUILDROOT CONFIG \n"
+	echo -e "\n TO FORCE UPDATE, DELETE .config OR MAKE CHANGES USING make menuconfig AND BUILD AGAIN\n"
 	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 
 fi
