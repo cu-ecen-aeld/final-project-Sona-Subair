cfad7f6f1db4b9e6f21363a92b3322957192d529
peterbraganza <peterbraganza52@gmail.com>
2022-12-05 02:06:46 +0000

    new hash for accl

--- base_external/package/mma8452q_application_code/mma8452q_application_code.mk
+++ base_external/package/mma8452q_application_code/mma8452q_application_code.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 
-MMA8452Q_APPLICATION_CODE_VERSION = ef5d7d4b09a7d3a54b6a43f9d158a34e1cdb42ce
+MMA8452Q_APPLICATION_CODE_VERSION = 5c0870ed03a055fb0704cecadb75bae3ff052514
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
